37ae5db58ef757103a2edb2e9d34906903100ff4
jschanker <jschanker@users.noreply.github.com>
2022-06-18 04:34:21 +0000

    deploy: 9ad0e685969d98f3b4c116aab82d603bd57e975d

--- src/common/context.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const DeviceProvider = React.createContext({});
--- src/pages/Login/assets/GoogleIcon.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const GoogleSvg = require("./Google.svg");
-const GoogleIcon = () => {
-  return <img src={GoogleSvg} />;
-};
-export default GoogleIcon;
