4e9bd6321caafe3cfc470b6a1e857060e1477809
jschanker <jschanker@molloy.edu>
2022-06-26 11:26:22 -0400

    Update en.js

--- src/msg/en.js
+++ src/msg/en.js
@@ -2,6 +2,7 @@ const en = {};
 en.LEARN = "Learn";
 en.ABOUT = "About";
 en.GET_INVOLVED = "Get Involved";
+en.VOLUNTEER = "Volunteer"
 en.VOLUNTEER_WITH_US = "Volunteer with Us";
 
 export default en;
