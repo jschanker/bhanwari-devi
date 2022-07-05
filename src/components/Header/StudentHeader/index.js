06f73d01f39b034c18fdd994b221919b162d8db1
jschanker <jschanker@molloy.edu>
2022-07-05 09:33:28 -0400

    Fix/StudentHeader add return

--- src/components/Header/StudentHeader/index.js
+++ src/components/Header/StudentHeader/index.js
@@ -37,92 +37,94 @@ function StudentHeader({ leftDrawer, toggleDrawer, onlyRole }) {
     setLearn(null);
   };
 
-  <>
-    <Box
-      sx={{
-        flexGrow: 1,
-        display: {
-          xs: "none",
-          md: "flex",
-        },
-      }}
-    >
-      <MenuItem onClick={handleOpenLearn}>
-        <Typography variant="subtitle1">
-          <Message constantKey={MENU_ITEMS[LEARN_KEY].msgKey} />
-        </Typography>
-        {learn ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-      </MenuItem>
-      <DropDown
-        dropDown={LEARN_KEY}
-        indicator={learn}
-        handleClose={handleCloseLearn}
-        toggleDrawer={toggleDrawer}
-      />
+  return (
+    <>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: {
+            xs: "none",
+            md: "flex",
+          },
+        }}
+      >
+        <MenuItem onClick={handleOpenLearn}>
+          <Typography variant="subtitle1">
+            <Message constantKey={MENU_ITEMS[LEARN_KEY].msgKey} />
+          </Typography>
+          {learn ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        </MenuItem>
+        <DropDown
+          dropDown={LEARN_KEY}
+          indicator={learn}
+          handleClose={handleCloseLearn}
+          toggleDrawer={toggleDrawer}
+        />
 
-      <HeaderNavLink
-        to={PATHS.NEWUSER_DASHBOARED}
-        text={<Message constantKey="DASHBOARD" />}
-        toggleDrawer={toggleDrawer}
-      />
-      <HeaderNavLink
-        to={PATHS.MENTOR}
-        text="Mentor"
-        toggleDrawer={toggleDrawer}
-      />
-    </Box>
-    <Box
-      sx={{
-        display: { xs: "block", md: "none" },
-      }}
-    >
-      <MobileDropDown
-        menuKey={LEARN_KEY}
-        handleClose={handleCloseLearn}
-        toggleDrawer={toggleDrawer}
-      />
-      <HeaderNavLink
-        to={PATHS.NEWUSER_DASHBOARED}
-        text={<Message constantKey="DASHBOARD" />}
-        toggleDrawer={toggleDrawer}
-      />
-      <HeaderNavLink
-        to={PATHS.MENTOR}
-        text="Mentor"
-        toggleDrawer={toggleDrawer}
-      />
-    </Box>
+        <HeaderNavLink
+          to={PATHS.NEWUSER_DASHBOARED}
+          text={<Message constantKey="DASHBOARD" />}
+          toggleDrawer={toggleDrawer}
+        />
+        <HeaderNavLink
+          to={PATHS.MENTOR}
+          text="Mentor"
+          toggleDrawer={toggleDrawer}
+        />
+      </Box>
+      <Box
+        sx={{
+          display: { xs: "block", md: "none" },
+        }}
+      >
+        <MobileDropDown
+          menuKey={LEARN_KEY}
+          handleClose={handleCloseLearn}
+          toggleDrawer={toggleDrawer}
+        />
+        <HeaderNavLink
+          to={PATHS.NEWUSER_DASHBOARED}
+          text={<Message constantKey="DASHBOARD" />}
+          toggleDrawer={toggleDrawer}
+        />
+        <HeaderNavLink
+          to={PATHS.MENTOR}
+          text="Mentor"
+          toggleDrawer={toggleDrawer}
+        />
+      </Box>
 
-    <Box
-      sx={{
-        display: { xs: "block", md: "flex" },
-        justifyContent: { xs: "normal", md: "flex-end" },
-        width: { xs: 0, sm: "100%" },
-        pr: onlyRole && 2,
-      }}
-    >
-      {!leftDrawer && (
-        <Link to={PATHS.SEARCHED_COURSE}>
-          <Tooltip title="Search the course...">
-            <Button color="dark">
-              <SearchIcon />
-            </Button>
-          </Tooltip>
-        </Link>
-      )}
+      <Box
+        sx={{
+          display: { xs: "block", md: "flex" },
+          justifyContent: { xs: "normal", md: "flex-end" },
+          width: { xs: 0, sm: "100%" },
+          pr: onlyRole && 2,
+        }}
+      >
+        {!leftDrawer && (
+          <Link to={PATHS.SEARCHED_COURSE}>
+            <Tooltip title="Search the course...">
+              <Button color="dark">
+                <SearchIcon />
+              </Button>
+            </Tooltip>
+          </Link>
+        )}
 
-      <HeaderNavLink
-        to={PATHS.ADMISSION}
-        text="Navgurukul Admission"
-        toggleDrawer={toggleDrawer}
-      />
-      <HeaderNavLink
-        to={PATHS.OPPORTUNITIES}
-        text={<Message constantKey="OPPORTUNITIES" />}
-        toggleDrawer={toggleDrawer}
-      />
-    </Box>
-  </>
+        <HeaderNavLink
+          to={PATHS.ADMISSION}
+          text="Navgurukul Admission"
+          toggleDrawer={toggleDrawer}
+        />
+        <HeaderNavLink
+          to={PATHS.OPPORTUNITIES}
+          text={<Message constantKey="OPPORTUNITIES" />}
+          toggleDrawer={toggleDrawer}
+        />
+      </Box>
+    </>
+  );
 }
 
 export default StudentHeader;
