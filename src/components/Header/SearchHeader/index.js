616d3eeb717df0d36b33f1749735ebaf9b2899e8
jschanker <jschanker@molloy.edu>
2022-07-05 20:54:57 -0400

    Fix/import SearchIcon in SearchHeader

--- src/components/Header/SearchHeader/index.js
+++ src/components/Header/SearchHeader/index.js
@@ -4,6 +4,7 @@ import HeaderNavLink from "../HeaderNavlink";
 import Message from "../../common/Message";
 import { PATHS } from "../../../constant";
 import Tooltip from "@mui/material/Tooltip";
+import SearchIcon from "@mui/icons-material/Search";
 import {
   Box,
   // Typography,
