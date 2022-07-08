efa605f270f6e652caa6bf4948c3feb5fdd08eee
jschanker <jschanker@molloy.edu>
2022-07-08 00:31:54 -0400

    Add required constant dependencies for selectRolesData

--- src/components/User/redux/selectors.js
+++ src/components/User/redux/selectors.js
@@ -1,5 +1,59 @@
+export const ADMIN_ROLE_KEY = "ADMIN";
+export const STUDENT_ROLE_KEY = "STUDENT";
+export const VOLUNTEER_ROLE_KEY = "VOLUNTEER";
+export const PARTNER_ROLE_KEY = "PARTNER";
+export const PARTNER_VIEW_ROLE_KEY = "PARTNER_VIEW";
+export const PARTNER_EDIT_ROLE_KEY = "PARTNER_EDIT";
+
+const ROLES = {
+  [ADMIN_ROLE_KEY]: {
+    msgKey: ADMIN_ROLE_KEY,
+    savedValue: "admin",
+    isDefault: false,
+  },
+  [STUDENT_ROLE_KEY]: {
+    msgKey: STUDENT_ROLE_KEY,
+    savedValue: "student",
+    isDefault: true,
+  },
+  [VOLUNTEER_ROLE_KEY]: {
+    msgKey: VOLUNTEER_ROLE_KEY,
+    savedValue: "volunteer",
+    isDefault: false,
+  },
+  [PARTNER_ROLE_KEY]: {
+    msgKey: PARTNER_ROLE_KEY,
+    savedValue: "partner",
+    isDefault: false,
+    properties: {
+      partnerId: null,
+      partnerGroupId: null,
+    }, 
+  },
+  [PARTNER_VIEW_ROLE_KEY]: {
+    msgKey: PARTNER_ROLE_KEY,
+    savedValue: "partner_view",
+    isDefault: false,
+  },
+  [PARTNER_EDIT_ROLE_KEY]: {
+    msgKey: PARTNER_ROLE_KEY,
+    savedValue: "partner_edit",
+    isDefault: false,
+  },
+};
+
+// keys of roles which every user has
+const DEFAULT_ROLES = Object.keys(ROLES).filter(
+  (role) => ROLES[role].isDefault
+);
+
+const savedRolesToKeysMap = Object.keys(ROLES).reduce((roleKeyMap, roleKey) => {
+  roleKeyMap[ROLES[roleKey].savedValue] = roleKey;
+  return roleKeyMap;
+}, {});
+
 /**
- * Custom hook to get user roles
+ * Selector to get user roles
  * @return {Array.<{key: string, msgKey: string, assignedRole:boolean}>}
  */
 export const selectRolesData = ({ User }) => {
