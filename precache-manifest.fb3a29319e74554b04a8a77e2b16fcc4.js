f26d7b49db41d8bbe69bd8e672720081e384ee6b
jschanker <jschanker@users.noreply.github.com>
2024-06-27 05:40:51 +0000

    deploy: 14b2e3527796e1d7ffe2eb90974747e08d278200

---
+++ precache-manifest.fb3a29319e74554b04a8a77e2b16fcc4.js
@@ -0,0 +1,418 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de5e0f6f3eb2b64014345e98ae8c424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2734f23262b9ef4c9cdb",
+    "url": "/static/css/2.373962c5.chunk.css"
+  },
+  {
+    "revision": "b37fe1dd4d56fce90f29",
+    "url": "/static/css/main.7bceaeee.chunk.css"
+  },
+  {
+    "revision": "2734f23262b9ef4c9cdb",
+    "url": "/static/js/2.0b152c88.chunk.js"
+  },
+  {
+    "revision": "16c36f3a7ad092b6956a55e95d5871b1",
+    "url": "/static/js/2.0b152c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37fe1dd4d56fce90f29",
+    "url": "/static/js/main.b5d4fcad.chunk.js"
+  },
+  {
+    "revision": "21c8cde6782351d65047",
+    "url": "/static/js/runtime-main.c5f31bc3.js"
+  },
+  {
+    "revision": "c46399b240558617b879bf1f91619266",
+    "url": "/static/media/ClassCreated.c46399b2.svg"
+  },
+  {
+    "revision": "ed255ec6d8cdd73c65957543d55e3333",
+    "url": "/static/media/ComputerScreen.ed255ec6.svg"
+  },
+  {
+    "revision": "e17d7bf40cb68f8b202543307689e2fe",
+    "url": "/static/media/ContentTypeCompleted.e17d7bf4.svg"
+  },
+  {
+    "revision": "3861449b7cab7ca3b606a23517326dbc",
+    "url": "/static/media/Email.3861449b.svg"
+  },
+  {
+    "revision": "91c10f90beaffb4f975d3f144f0cd10a",
+    "url": "/static/media/ErrorDog.91c10f90.svg"
+  },
+  {
+    "revision": "f312c4df33cc4c62e8df0b3e15928e35",
+    "url": "/static/media/ExerciseCompleted.f312c4df.svg"
+  },
+  {
+    "revision": "3b55f0882c5c8555b5f2fa5a82bd972c",
+    "url": "/static/media/Google.3b55f088.svg"
+  },
+  {
+    "revision": "426d45f56745bd530762daa452b19197",
+    "url": "/static/media/Group.426d45f5.svg"
+  },
+  {
+    "revision": "84865e9436b8ab80eafaf6aca3163f8a",
+    "url": "/static/media/Group.84865e94.svg"
+  },
+  {
+    "revision": "ad76898fa42d24ce26299aee6047750a",
+    "url": "/static/media/Group.ad76898f.svg"
+  },
+  {
+    "revision": "5ec4176b62567903706b039c9290b233",
+    "url": "/static/media/Layer.5ec4176b.svg"
+  },
+  {
+    "revision": "80ac371c0ed069fdfe1235fa2bca8fad",
+    "url": "/static/media/NoBatchesjpg.80ac371c.jpg"
+  },
+  {
+    "revision": "429e5b1b4181d7986d5373a0e4ed356d",
+    "url": "/static/media/NoCourses.429e5b1b.svg"
+  },
+  {
+    "revision": "4862977292c76e9efbe0af9e28780a4a",
+    "url": "/static/media/NoRevision.48629772.svg"
+  },
+  {
+    "revision": "4fd1f36d195108c1f2a941d883e53f95",
+    "url": "/static/media/Second-img.4fd1f36d.svg"
+  },
+  {
+    "revision": "87806a53079fe418a0e90183beb0486e",
+    "url": "/static/media/Vector.87806a53.svg"
+  },
+  {
+    "revision": "c9bece636e2efaef1627038a665fb544",
+    "url": "/static/media/Vector.c9bece63.svg"
+  },
+  {
+    "revision": "87ceb8f6996811e26c2205be1a67b88d",
+    "url": "/static/media/amg.87ceb8f6.svg"
+  },
+  {
+    "revision": "a74bfabab5c11615664878b5cbd9074c",
+    "url": "/static/media/amzbootcamp.a74bfaba.svg"
+  },
+  {
+    "revision": "ea52e90295d2bd1dca600a5d479449a6",
+    "url": "/static/media/android.ea52e902.svg"
+  },
+  {
+    "revision": "1168bba0e074283a412ae88b85be3f3b",
+    "url": "/static/media/assessment.1168bba0.svg"
+  },
+  {
+    "revision": "f312c4df33cc4c62e8df0b3e15928e35",
+    "url": "/static/media/assessmentCompleted.f312c4df.svg"
+  },
+  {
+    "revision": "2c5d7b645dc3f44cd2b914225bfe49f0",
+    "url": "/static/media/assessmentRevisit.2c5d7b64.svg"
+  },
+  {
+    "revision": "48d5527690ee4f3b8b271fa026e4be4a",
+    "url": "/static/media/assessmentSelected.48d55276.svg"
+  },
+  {
+    "revision": "c4adb9c8000fd31d17c17e157b613ff1",
+    "url": "/static/media/butterfly.c4adb9c8.svg"
+  },
+  {
+    "revision": "ed7f8251dcf8ec131758bfe6dde8aef0",
+    "url": "/static/media/calendar.ed7f8251.svg"
+  },
+  {
+    "revision": "31c9e0993025cd87df0891b0a1469f28",
+    "url": "/static/media/calender.31c9e099.svg"
+  },
+  {
+    "revision": "b448278f13110e829fe284be408946d0",
+    "url": "/static/media/certificate-color.b448278f.svg"
+  },
+  {
+    "revision": "26c54b121f59ea5088a585ea3974765f",
+    "url": "/static/media/certificate-grey.26c54b12.svg"
+  },
+  {
+    "revision": "265bfa6e61640aa2c83059750a44e6b8",
+    "url": "/static/media/certificate.265bfa6e.svg"
+  },
+  {
+    "revision": "176628a4599a1f62b906c98d3275e661",
+    "url": "/static/media/classTypeCompleted.176628a4.svg"
+  },
+  {
+    "revision": "b3c8758857209139e2816cadd8a5c04f",
+    "url": "/static/media/classTypeRevisit.b3c87588.svg"
+  },
+  {
+    "revision": "8d0f253fd9534c5009a22a3c7d7bc2ab",
+    "url": "/static/media/classTypeSelected.8d0f253f.svg"
+  },
+  {
+    "revision": "b8e1b92e98bef84071c6da602a7bb4ae",
+    "url": "/static/media/classtype.b8e1b92e.svg"
+  },
+  {
+    "revision": "593998efd1dc48c79dcaf0914a1f82bf",
+    "url": "/static/media/code-example.593998ef.svg"
+  },
+  {
+    "revision": "58d0e1cea45c113d5127733a762ee380",
+    "url": "/static/media/completed_task.58d0e1ce.svg"
+  },
+  {
+    "revision": "4430b5911c45650373ae1e17b5e19bcb",
+    "url": "/static/media/contentTypeRevist.4430b591.svg"
+  },
+  {
+    "revision": "57dc9bc471a1551110d5899a7d49b30f",
+    "url": "/static/media/contentTypeSelected.57dc9bc4.svg"
+  },
+  {
+    "revision": "a87fd66377c5d916b8714e42ff86ba53",
+    "url": "/static/media/contenttype.a87fd663.svg"
+  },
+  {
+    "revision": "54daf7f9763ef9de58b440f43b695022",
+    "url": "/static/media/degree.54daf7f9.svg"
+  },
+  {
+    "revision": "1b9fb29b4c89e51da2c5669fbe25955a",
+    "url": "/static/media/dicto.1b9fb29b.jpg"
+  },
+  {
+    "revision": "ff236fd54c4f9f1af711049309ba7dcb",
+    "url": "/static/media/difficulty-english.ff236fd5.svg"
+  },
+  {
+    "revision": "3d3da03c1122f7607a425a5b515621a0",
+    "url": "/static/media/doublequote.3d3da03c.svg"
+  },
+  {
+    "revision": "aebabc55cc46bef5df702af00c069d73",
+    "url": "/static/media/down-swirly.aebabc55.svg"
+  },
+  {
+    "revision": "574116b4385a52e0b15667e26c11b8c7",
+    "url": "/static/media/dropdown.574116b4.svg"
+  },
+  {
+    "revision": "10ef3d73177fbbe31cd1b2a0f63897df",
+    "url": "/static/media/facebook.10ef3d73.svg"
+  },
+  {
+    "revision": "e854db315df9c2c80311fb8d2afa7f24",
+    "url": "/static/media/facilitator.e854db31.svg"
+  },
+  {
+    "revision": "c79b206266c9cfa675460c0d18fa7c04",
+    "url": "/static/media/fe.c79b2062.svg"
+  },
+  {
+    "revision": "fdbf530b86a666537c0ef5de1ef6da6f",
+    "url": "/static/media/girls.fdbf530b.svg"
+  },
+  {
+    "revision": "5040f6551cb4430c563d5ae6a121a9c3",
+    "url": "/static/media/graphic.5040f655.svg"
+  },
+  {
+    "revision": "a884469ab75ef4beb45476a0ebcfcd79",
+    "url": "/static/media/lang.a884469a.svg"
+  },
+  {
+    "revision": "79f2c64a3efa61c3106613b6b1a0601f",
+    "url": "/static/media/language.79f2c64a.svg"
+  },
+  {
+    "revision": "e4b76cde0be714625c935570e382163c",
+    "url": "/static/media/language.e4b76cde.svg"
+  },
+  {
+    "revision": "0185af4567706270014a8c19fe572020",
+    "url": "/static/media/learn-python.0185af45.svg"
+  },
+  {
+    "revision": "15517aab7b22bf2f548094109c2fe7be",
+    "url": "/static/media/leftquote.15517aab.svg"
+  },
+  {
+    "revision": "86fc7f5a90f963b56e05d489b6606914",
+    "url": "/static/media/linkedIn.86fc7f5a.svg"
+  },
+  {
+    "revision": "5f20b50bd05a02c23b3b12d08fe84795",
+    "url": "/static/media/linkedin.5f20b50b.svg"
+  },
+  {
+    "revision": "66938df1d3fec5529de156bc8850de12",
+    "url": "/static/media/livelessons.66938df1.svg"
+  },
+  {
+    "revision": "baac942abe3423c9d7e06e8b7cef543c",
+    "url": "/static/media/login illustration.baac942a.png"
+  },
+  {
+    "revision": "558f26358a399f0d692d12a73d08f3a7",
+    "url": "/static/media/logo.558f2635.svg"
+  },
+  {
+    "revision": "c77050dfc69a8d400a3391e8bc426cb5",
+    "url": "/static/media/logo.c77050df.svg"
+  },
+  {
+    "revision": "133268faa55f95716db0b91972f7601d",
+    "url": "/static/media/main_image.133268fa.svg"
+  },
+  {
+    "revision": "123e735056447ba03b609af18d2fa553",
+    "url": "/static/media/menu.123e7350.svg"
+  },
+  {
+    "revision": "dd9c8e0919942dd813be55a2b44bc300",
+    "url": "/static/media/meraki.dd9c8e09.svg"
+  },
+  {
+    "revision": "5b7c358bd51353c45826401a4fc69e64",
+    "url": "/static/media/misc.5b7c358b.svg"
+  },
+  {
+    "revision": "68759971def1807722b59894ddcc2a2f",
+    "url": "/static/media/misc.68759971.svg"
+  },
+  {
+    "revision": "3694d784a6e4107f3f18947e0e91afef",
+    "url": "/static/media/misce.3694d784.svg"
+  },
+  {
+    "revision": "660d1e583cb184013bbfc2a480896f1a",
+    "url": "/static/media/never-typed.660d1e58.svg"
+  },
+  {
+    "revision": "fe465642c922161e24e6acd9ef0a5f5b",
+    "url": "/static/media/notEnrolled.fe465642.svg"
+  },
+  {
+    "revision": "b16f857a7d9164c5d7e51a8afadb441f",
+    "url": "/static/media/playButton.b16f857a.svg"
+  },
+  {
+    "revision": "35f7af8f823519e52411d26e7810b130",
+    "url": "/static/media/playStore.35f7af8f.svg"
+  },
+  {
+    "revision": "74a3c8129642a4d07cc67fa3eeeb9c11",
+    "url": "/static/media/playstore.74a3c812.svg"
+  },
+  {
+    "revision": "5df8fc8d8670f9120cdd28429d9d3017",
+    "url": "/static/media/python.5df8fc8d.svg"
+  },
+  {
+    "revision": "7b2be4e59a002649d6ceadc8c9b483f6",
+    "url": "/static/media/python.7b2be4e5.svg"
+  },
+  {
+    "revision": "68759971def1807722b59894ddcc2a2f",
+    "url": "/static/media/random.68759971.svg"
+  },
+  {
+    "revision": "70fbd86eefb4531a91f3c17e92c29f0f",
+    "url": "/static/media/ratingIcon.70fbd86e.svg"
+  },
+  {
+    "revision": "fd2a4c60c02c3ab678e0e5f4787bfede",
+    "url": "/static/media/real_time.fd2a4c60.svg"
+  },
+  {
+    "revision": "f679e79a9a39ad8b9b84a87534868b6a",
+    "url": "/static/media/residential.f679e79a.svg"
+  },
+  {
+    "revision": "abf4ec239b0f3af85179a7749fdeb74d",
+    "url": "/static/media/scale.abf4ec23.svg"
+  },
+  {
+    "revision": "630838a34aa48b4cde8a7f40776fd6c0",
+    "url": "/static/media/scratch.630838a3.svg"
+  },
+  {
+    "revision": "f182bb7946d77b44a7d00bf0fda3519c",
+    "url": "/static/media/search.f182bb79.svg"
+  },
+  {
+    "revision": "adc0e0b99f692b5fa730a5696c175c5b",
+    "url": "/static/media/separator.adc0e0b9.svg"
+  },
+  {
+    "revision": "fda256b19cfb08554b158040360274f2",
+    "url": "/static/media/specialdeals.fda256b1.svg"
+  },
+  {
+    "revision": "19a047035f9e29788235e72426ab163d",
+    "url": "/static/media/tick.19a04703.svg"
+  },
+  {
+    "revision": "fba15db75c18cc35f8dd9507b9fa0206",
+    "url": "/static/media/time.fba15db7.svg"
+  },
+  {
+    "revision": "46291b283442e99e639383bec1fb4b25",
+    "url": "/static/media/twitter.46291b28.svg"
+  },
+  {
+    "revision": "a0cbc23565694339307db976d838073a",
+    "url": "/static/media/twitter.a0cbc235.svg"
+  },
+  {
+    "revision": "f6cd8593af91c97d80fa1fcc5f3f76f4",
+    "url": "/static/media/typeing.f6cd8593.svg"
+  },
+  {
+    "revision": "2fdee830012a13e807f5d11d5d20f91c",
+    "url": "/static/media/typing.2fdee830.svg"
+  },
+  {
+    "revision": "f101187606ff9144a7a8cd3ca999a3c1",
+    "url": "/static/media/undraw_agreement.f1011876.svg"
+  },
+  {
+    "revision": "f409d323e621a030b8ff48fe4fd3b635",
+    "url": "/static/media/user-1.f409d323.png"
+  },
+  {
+    "revision": "06b8ea68054176843ecd6b13f34b6159",
+    "url": "/static/media/user-2.06b8ea68.png"
+  },
+  {
+    "revision": "9305cc84ff5529b78381aff816f66273",
+    "url": "/static/media/user-3.9305cc84.png"
+  },
+  {
+    "revision": "55e72a9320e0baac5e1229f357572f40",
+    "url": "/static/media/web-development.55e72a93.svg"
+  },
+  {
+    "revision": "81bddddd6582d60bc517553ab790e668",
+    "url": "/static/media/web.81bddddd.svg"
+  },
+  {
+    "revision": "4bc5f357ed77eb2b9662c10c07b8a4a7",
+    "url": "/static/media/whatsapp.4bc5f357.svg"
+  },
+  {
+    "revision": "64d0103a5aa983cb0126c74a81fb5533",
+    "url": "/static/media/world_icon.64d0103a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bhanwari-devi/precache-manifest.514e623eb0999d022ae9e2325f4a277b.js"
+  "/precache-manifest.fb3a29319e74554b04a8a77e2b16fcc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bhanwari-devi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
