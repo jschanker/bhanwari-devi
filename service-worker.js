9300c275247bee9837be6a1a1f8bdba30834a6b7
jschanker <jschanker@users.noreply.github.com>
2024-06-27 05:54:50 +0000

    deploy: b58e4d7a2558699e05ddf7950f61223d88e4d280

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9ab7808f248c09fa39a5ca320e064c5.js"
+  "/bhanwari-devi/precache-manifest.5bf0be8e908342f76d987414d6783268.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bhanwari-devi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
